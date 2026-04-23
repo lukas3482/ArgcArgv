f25a0761401156dde99c8e2f62ede2191ae5d369
Lukas Wolf <wolflukas@outlook.at>
2026-04-23 13:47:02 +0000

    Update argv.c

--- argv.c
+++ argv.c
@@ -6,6 +6,16 @@ int main(int argc, char *argv[]) {
 
   printf("What the heck are these argc and argv?\n");
   printf("Why is one an int and one an array?\n");
+
+  printf("Number of Args: %d\n", argc);
+  printf("Args: \n");
+  for(int i = 0; i < argc; i++){
+    printf("%s\n", argv[i]);
+  }
+
+
+  printf("Number of words: %d\n", wordcount)
+
   
   return 0;
 }
